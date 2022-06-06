6a9eee5a849736745ab5ae8ee6ebef16c2b1a511
Sushant-2 <sushant.chaudhary123@gmail.com>
2022-06-06 22:30:16 +0530

    day5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/pages/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'pages/home_page.dart';
-
 import 'package:flutter_catalog/pages/home_page.dart';
 
 void main() {
@@ -17,12 +16,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       
+      
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.green, fontFamily: GoogleFonts.lato().fontFamily),
-      
       darkTheme: ThemeData(brightness: Brightness.dark),
       // initialRoute: "/home",
-      routes: {"/": (context) => LoginPage(), "/home": (context) => HomePage()},
+      routes: {"/": (context) => LoginPage(), MyRoutes.homeRoute:(context) => HomePage(),   MyRoutes.loginRoute: (context) => LoginPage()},
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/utils/routes.dart';
+
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,6 +9,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Colors.white,
       child: Column(
         children: [
           Image.asset("assets/images/login_pic.png"),
@@ -21,7 +25,9 @@ class LoginPage extends StatelessWidget {
             ),
             TextFormField( obscureText: true , decoration: InputDecoration(hintText: "Enter Password", labelText: "Password"),),
             SizedBox(height: 20),
-            ElevatedButton(child: Text("Login"),style: TextButton.styleFrom(),onPressed: (){print("Hello Sushant");})
+            ElevatedButton(child: Text("Login"),style: TextButton.styleFrom(minimumSize: Size(90, 35) ), onPressed: (){ 
+              Navigator.pushNamed(context, MyRoutes.homeRoute);
+            }  )
             
               ],
             ),
