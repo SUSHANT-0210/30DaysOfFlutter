ba36ed7c8c58a864318ac771f4ab4af203de6d8a
Sushant-2 <sushant.chaudhary123@gmail.com>
2022-06-21 22:43:27 +0530

    Day 27 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -29,11 +30,9 @@ class CartPage extends StatelessWidget {
 
 class _CartTotal extends StatelessWidget {
   
-  final _cart = CartModel();
-
-
   @override
   Widget build(BuildContext context) {
+    final CartModel _cart = (VxState.store as MyStore).cart;
     return SizedBox(
       height: 200,
       child: Row(
@@ -57,9 +56,10 @@ class _CartTotal extends StatelessWidget {
 
 
 class _CartList extends StatelessWidget {
-  final _cart = CartModel();
+  
   @override
   Widget build(BuildContext context) {
+    final CartModel _cart = (VxState.store as MyStore).cart;
     return  _cart.items.isEmpty?"Nothing to show".text.xl3.makeCentered() : ListView.builder(
       itemCount: _cart.items.length,
       itemBuilder: (context,index)=>ListTile(
